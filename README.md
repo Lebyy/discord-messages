55398ea20641e2e7dd38870dfa8743941b95388d
Lebyy Dev <lebon1377@gmail.com>
2021-03-14 14:35:51 +0000

    🎉 Release v1.0.0

---
+++ README.md
@@ -0,0 +1,164 @@
+<p align="center"><a href="https://nodei.co/npm/discord-messages/"><img src="https://nodei.co/npm/discord-messages.png"></a></p>
+<p align="center"><img src="https://img.shields.io/npm/v/discord-messages"> <img src="https://img.shields.io/github/repo-size/Lebyy/discord-messages"> <img src="https://img.shields.io/npm/l/discord-messages"> <img src="https://img.shields.io/github/contributors/Lebyy/discord-messages"> <img src="https://img.shields.io/github/package-json/dependency-version/Lebyy/discord-messages/mongoose"> <a href="https://discord.gg/pndumb6J3t"><img src="https://discordapp.com/api/guilds/815261972450115585/widget.png" alt="Discord server"/></a></p>
+
+# Discord-Messages
+- A lightweight and easy to use messages framework for discord bots, uses MongoDB.
+- If you need help feel free to join our <a href="https://discord.gg/pndumb6J3t">discord server</a> to talk and help you with your code.
+- If you encounter any of those fell free to open an issue in our <a href="https://github.com/Lebyy/discord-messages/issues">github repository</a>.
+
+# Download & Update
+You can download it from npm:
+```cli
+npm i discord-messages
+```
+You can update to a newer version to receive updates using npm.
+```cli
+npm update discord-messages
+```
+
+# Setting Up
+First things first, we include the module into the project.
+```js
+const Messages = require("discord-xp");
+```
+After that, you need to provide a valid mongo database url, and set it. You can do so by:
+```js
+Messages.setURL("mongodb://..."); // You only need to do this ONCE per process.
+```
+
+# Examples
+*Examples assume that you have setted up the module as presented in 'Setting Up' section.*
+*Following examples assume that your `Discord.Client` is called `client`.*
+
+*Following examples assume that your `client.on("message", message` is called `message`.*
+
+*Following example contains isolated code which you need to integrate in your own command handler.*
+
+*Following example assumes that you are able to write asynchronous code (use `await`).*
+
+- **Adding a message to user's database for each Message sent**
+
+```js
+client.on("message", async (message) => {
+  if (!message.guild) return;
+  if (message.author.bot) return;
+
+  const AddMessage = await Messages.appendMessage(message.author.id, message.guild.id, 1);
+});
+```
+- **Rank Command**
+
+```js
+const target = message.mentions.users.first() || message.author; // Grab the target.
+
+const user = await Messages.fetch(target.id, message.guild.id); // Selects the target from the database.
+
+if (!user) return message.channel.send("Seems like this user does not any messages so far..."); // If there isnt such user in the database, we send a message in general.
+
+message.channel.send(`> **${target.tag}** currently has ${user.data.messages} message(s).`); // We show the message(s) count.
+```
+
+- **Leaderboard Command**
+
+```js
+const rawLeaderboard = await Messages.fetchLeaderboard(message.guild.id, 10); // We grab top 10 users with most message(s) in the current server.
+
+if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
+
+const leaderboard = await Messages.computeLeaderboard(client, rawLeaderboard, true); // We process the leaderboard.
+
+const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nMessages Count: ${e.messages}`); // We map the outputs.
+
+message.channel.send(`**Leaderboard**:\n\n${lb.join("\n\n")}`);
+```
+
+*Is time for you to get creative..*
+
+# Methods
+**createUser**
+
+Creates an entry in database for that user if it doesnt exist.
+```js
+Messages.createUser(<UserID - String>, <GuildID - String>);
+```
+- Output:
+```
+Promise<Object>
+```
+**deleteUser**
+
+If the entry exists, it deletes it from database.
+```js
+Messages.deleteUser(<UserID - String>, <GuildID - String>);
+```
+- Output:
+```
+Promise<Object>
+```
+**appendMessage**
+
+It adds a specified amount of messages to the current amount of messages for that user, in that guild. It creates a new user with that amount of messages, if there is no entry for that user. 
+```js
+Messages.appendMessage(<UserID - String>, <GuildID - String>, <Amount - Integer>);
+```
+- Output:
+```
+Promise<Boolean>
+```
+**setMessage**
+
+It sets the messages to a specified amount and re-calculates the level.
+```js
+Messages.setMessages(<UserID - String>, <GuildID - String>, <Amount - Integer>);
+```
+- Output:
+```
+Promise<Boolean/Object>
+```
+**fetch**
+
+Retrives selected entry from the database, if it exists.
+```js
+Messages.fetch(<UserID - String>, <GuildID - String>, <FetchPosition - Boolean>);
+```
+- Output:
+```
+Promise<Object>
+```
+**subtractMessages**
+
+It removes a specified amount of messages to the current amount of messages for that user, in that guild.
+```js
+Messages.subtractXp(<UserID - String>, <GuildID - String>, <Amount - Integer>);
+```
+- Output:
+```
+Promise<Boolean/Object>
+```
+**fetchLeaderboard**
+
+It gets a specified amount of entries from the database, ordered from higgest to lowest within the specified limit of entries.
+```js
+Messages.fetchLeaderboard(<GuildID - String>, <Limit - Integer>);
+```
+- Output:
+```
+Promise<Array [Objects]>
+```
+**computeLeaderboard**
+
+It returns a new array of object that include messages, guild id, user id, leaderboard position, username and discriminator.
+```js
+Messages.computeLeaderboard(<Client - Discord.js Client>, <Leaderboard - fetchLeaderboard output>, <fetchUsers - boolean, disabled by default>);
+```
+- Output:
+```
+Promise<Array [Objects]>
+```
+
+# Enjoying Discord-Messages?
+Consider donating: https://www.buymeacoffee.com/lebyydev
+Thanks 😊
+
+# Credits
+- [Discord-XP](https://github.com/MrAugu/discord-xp) Discord-Messages wouldn't exist without them! Consider to check out Discord-XP package aswell and star the repo!
---
+++ index.js
@@ -0,0 +1,255 @@
+const mongoose = require("mongoose");
+const Messages = require("./models/messages.js");
+var mongoUrl;
+
+/**
+ *
+ *
+ * @class DiscordMessages
+ */
+class DiscordMessages {
+
+  /**
+   *
+   *
+   * @static
+   * @param {string} [dbUrl] - A valid mongo database URI.
+   * @return {Promise} - The mongoose connection promise.
+   * @memberof DiscordMessages
+   */
+  static async setURL(dbUrl) {
+    if (!dbUrl) throw new TypeError("A database url was not provided.");
+    mongoUrl = dbUrl;
+    return mongoose.connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {string} [userId] - Discord user id.
+   * @param {string} [guildId] - Discord guild id.
+   * @return {objcet} - The user data object.
+   * @memberof DiscordMessages
+   */
+  static async createUser(userId, guildId) {
+    if (!userId) throw new TypeError("An user id was not provided.");
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+
+    const isUser = await Messages.findOne({ userID: userId, guildID: guildId });
+    if (isUser) return false;
+
+    const newUser = new Messages({
+      userID: userId,
+      guildID: guildId
+    });
+
+    await newUser.save().catch(e => console.log(`Failed to create user: ${e}`));
+
+    return newUser;
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {string} [userId] - Discord user id.
+   * @param {string} [guildId] - Discord guild id.
+   * @return {objcet} - The user data object.
+   * @memberof DiscordMessages
+   */
+  static async deleteUser(userId, guildId) {
+    if (!userId) throw new TypeError("An user id was not provided.");
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+
+    const user = await Messages.findOne({ userID: userId, guildID: guildId });
+    if (!user) return false;
+
+    await Messages.findOneAndDelete({ userID: userId, guildID: guildId }).catch(e => console.log(`Failed to delete user: ${e}`));
+
+    return user;
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {string} [userId] - Discord user id.
+   * @param {string} [guildId] - Discord guild id.
+   * @param {number} [messages] - Amount of messages to append.
+   * @return {objcet} - The user data object. 
+   * @memberof DiscordMessages
+   */
+  static async appendMessage(userId, guildId, messages) {
+    if (!userId) throw new TypeError("An user id was not provided.");
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+    if (messages == 0 || !messages || isNaN(parseInt(messages))) throw new TypeError("An amount of messages was not provided/was invalid.");
+		const user = await Messages.findOne({ userID: userId, guildID: guildId });
+
+    if (!user) {
+      const newUser = new Messages({
+        userID: userId,
+        guildID: guildId,
+        messages: messages
+    });
+
+      await newUser.save().catch(e => console.log(`Failed to save new user.`));
+
+      return messages;
+    };
+
+    user.messages += parseInt(messages, 10);
+
+    await user.save().catch(e => console.log(`Failed to append messages: ${e}`) );
+
+    return user.messages
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {string} [userId] - Discord user id.
+   * @param {string} [guildId] - Discord guild id.
+   * @param {number} [messages] - Amount of messages to set.
+   * @return {objcet} - The user data object.
+   * @memberof DiscordMessages
+   */
+  static async setMessages(userId, guildId, messages) {
+    if (!userId) throw new TypeError("An user id was not provided.");
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+    if (messages == 0 || !messages || isNaN(parseInt(messages))) throw new TypeError("An amount of messages was not provided/was invalid.");
+
+    const user = await Messages.findOne({ userID: userId, guildID: guildId });
+    if (!user) return false;
+
+    user.messages = messages;
+
+    user.save().catch(e => console.log(`Failed to set messages: ${e}`) );
+
+    return user;
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {string} [userId] - Discord user id.
+   * @param {string} [guildId] - Discord guild id.
+	 * @param {boolean} [fetchPosition=false] - Wheter to fetch the users position.
+   * @return {objcet} - The user data object.
+   * @memberof DiscordMessages
+   */
+  static async fetch(userId, guildId, fetchPosition = false) {
+    if (!userId) throw new TypeError("An user id was not provided.");
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+    const user = await Messages.findOne({
+      userID: userId,
+      guildID: guildId
+    });
+    if (!user) return false;
+		let userobj = {}
+    if (fetchPosition === true) {
+      const leaderboard = await Messages.find({
+        guildID: guildId
+      }).sort([['messages', 'descending']]).exec();
+      userobj.position = leaderboard.findIndex(i => i.userID === userId) + 1;
+    }
+		userobj.data = user
+    return userobj;
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {string} [userId] - Discord user id.
+   * @param {string} [guildId] - Discord guild id.
+   * @param {number} [messages] - Amount of messages to subtract.
+   * @return {objcet} - The user data object.
+   * @memberof DiscordMessages
+   */
+  static async subtractMessages(userId, guildId, messages) {
+    if (!userId) throw new TypeError("An user id was not provided.");
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+    if (messages == 0 || !messages || isNaN(parseInt(messages))) throw new TypeError("An amount of messages was not provided/was invalid.");
+
+    const user = await Messages.findOne({ userID: userId, guildID: guildId });
+    if (!user) return false;
+
+    user.messages -= messages;
+
+    user.save().catch(e => console.log(`Failed to subtract messages: ${e}`) );
+
+    return user;
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {string} [guildId] - Discord guild id.
+   * @param {number} [limit] - Amount of maximum enteries to return.
+   * @return {Array} - The leaderboard array.
+   * @memberof DiscordMessages
+   */
+  static async fetchLeaderboard(guildId, limit) {
+    if (!guildId) throw new TypeError("A guild id was not provided.");
+    if (!limit) throw new TypeError("A limit was not provided.");
+
+    var users = await Messages.find({ guildID: guildId }).sort([['messages', 'descending']]).exec();
+
+    return users.slice(0, limit);
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {string} [client] - Your Discord.CLient.
+   * @param {array} [leaderboard] - The output from 'fetchLeaderboard' function.
+   * @param {boolean} [fetchUsers=false] - Wheter to fetch each users position.
+   * @return {*} 
+   * @memberof DiscordMessages
+   */
+  static async computeLeaderboard(client, leaderboard, fetchUsers = false) {
+    if (!client) throw new TypeError("A client was not provided.");
+    if (!leaderboard) throw new TypeError("A leaderboard id was not provided.");
+
+    if (leaderboard.length < 1) return [];
+
+    const computedArray = [];
+
+    if (fetchUsers) {
+      for (const key of leaderboard) {
+        const user = await client.users.fetch(key.userID) || { username: "Unknown", discriminator: "0000" };
+        computedArray.push({
+          guildID: key.guildID,
+          userID: key.userID,
+          messages: key.messages,
+          position: (leaderboard.findIndex(i => i.guildID === key.guildID && i.userID === key.userID) + 1),
+          username: user.username,
+          discriminator: user.discriminator
+        });
+      }
+    } else {
+      leaderboard.map(key => computedArray.push({
+        guildID: key.guildID,
+        userID: key.userID,
+        messages: key.messages,
+        position: (leaderboard.findIndex(i => i.guildID === key.guildID && i.userID === key.userID) + 1),
+        username: client.users.cache.get(key.userID) ? client.users.cache.get(key.userID).username : "Unknown",
+        discriminator: client.users.cache.get(key.userID) ? client.users.cache.get(key.userID).discriminator : "0000"
+      }));
+    }
+
+    return computedArray;
+  }
+
+}
+
+module.exports = DiscordMessages;
